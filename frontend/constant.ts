58e616f27c247ca5fe3328ba4f90ecab6027626e
jesse-lucus <jesselucus.contact@gmail.com>
2022-04-01 04:19:11 +0900

    push all terra dapp structure

---
+++ frontend/constant.ts
@@ -0,0 +1,11 @@
+const NETWORK = "https://bombay-lcd.terra.dev";
+const CHAIN_ID = "bombay-12";
+// const WALLET_SEEDS = "syrup remind amazing enable where salon amazing hope teach faith mansion draw six hope luggage promote false waste swarm sell urge park entry super";
+// const WALLET_SEEDS = "meat dash settle scheme country ripple amateur autumn edit reunion tilt state penalty genius toilet enhance pull boy essay sausage vacant grace wood curtain";
+const WALLET_SEEDS = "choose tragic brother survey neutral jump away left real tortoise license sheriff slow bulb situate seek fatigue seven noise neither excite analyst panda work";
+
+export default {
+  NETWORK,
+  CHAIN_ID,
+  WALLET_SEEDS,
+};
---
+++ keys.terrain.js
@@ -0,0 +1,9 @@
+// can use `process.env.SECRET_MNEMONIC` or `process.env.SECRET_PRIV_KEY`
+// to populate secret in CI environment instead of hardcoding
+
+module.exports = {
+  "bombay-12": {
+    mnemonic:
+      "",
+  }
+};
